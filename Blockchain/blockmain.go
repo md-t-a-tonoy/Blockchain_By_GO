1732fb811ac99e05cf5c307cd030c88bc4459c8c
Md Thouhedul Alam Tonoy <117182746+md-t-a-tonoy@users.noreply.github.com>
2023-10-07 22:41:55 +0530

    Add files via upload

---
+++ Blockchain/blockmain.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	block "main/Blockchain/Block"
+	wallet "main/Blockchain/Wallet"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
+
+	// Wallet
+	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+
+	// Blockchain
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
+		walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added? ", isAdded)
+}
